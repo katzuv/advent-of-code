7c36fc755da07799ab0a87531fb38e084a321c7e
Dan Katzuv <dan.dan.kafe@gmail.com>
2019-12-05 23:06:27 +0200

    Move code into function

--- 2019/d02/p1.py
+++ 2019/d02/p1.py
@@ -1,20 +1,27 @@
-if __name__ == '__main__':
+def program_output(numbers):
+    numbers[1] = 12
+    numbers[2] = 2
+    for i in range(0, len(numbers), 4):
+        opcode = numbers[i]
+        if opcode == 99:
+            break
+        first = numbers[numbers[i + 1]]
+        second = numbers[numbers[i + 2]]
+        output_index = numbers[i + 3]
+        if opcode == 1:
+            numbers[output_index] = first + second
+        elif opcode == 2:
+            numbers[output_index] = first * second
+    return numbers
+
+
+def main():
     with open('../inputs/2.txt') as input_file:
         numbers = [int(number) for number in input_file.read().split(',')]
+    output = program_output(numbers)
+    print(numbers)
+    print(f'Value at position 0: {output[0]}')
 
-        numbers[1] = 12
-        numbers[2] = 2
 
-        for i in range(0, len(numbers), 4):
-            opcode = numbers[i]
-            if opcode == 99:
-                break
-            first = numbers[numbers[i + 1]]
-            second = numbers[numbers[i + 2]]
-            output_index = numbers[i + 3]
-            if opcode == 1:
-                numbers[output_index] = first + second
-            elif opcode == 2:
-                numbers[output_index] = first * second
-
-        print(f'Value at position 0: {numbers[0]}')
+if __name__ == '__main__':
+    main()
