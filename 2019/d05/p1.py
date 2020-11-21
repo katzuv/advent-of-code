0472fb0d0a4ff11490debc7725ceea4a7b43883b
Dan Katzuv <dan.dan.kafe@gmail.com>
2020-11-21 19:26:38 +0200

    Solve 2019 day 5 part 1

--- 2019/d05/p1.py
+++ 2019/d05/p1.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from typing import List
 
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -8,10 +7,10 @@ from d02.p1 import IntcodeComputer, get_program_from_input
 
 
 class ThermalEnvironmentSupervisionTerminal(IntcodeComputer):
-    def _run_opcode_3(self, parameters: List[int], modes: List[int]):
+    def _run_opcode_3(self, parameters: list[int], modes: list[int]):
         self.program[parameters[0]] = int(input('Enter input for opcode 3: '))
 
-    def _run_opcode_4(self, parameters: List[int], modes: List[int]):
+    def _run_opcode_4(self, parameters: list[int], modes: list[int]):
         address = parameters[0]
         print(f'Value at address {address}: {self.program[address]}')
 
@@ -19,20 +18,32 @@ class ThermalEnvironmentSupervisionTerminal(IntcodeComputer):
         i = 0
         while True:
             instruction = str(self.program[i])
-            opcode = int(instruction[-2:])
+            opcode = self._get_opcode_from_instruction(instruction)
             if opcode == 99:
                 return
             modes = self._pad_modes(instruction, opcode)
             self._run_instruction(opcode, self.program[i + 1: i + 1 + self.OPCODES_TO_AMOUNT_OF_PARAMS[opcode]], modes)
 
             i += self.OPCODES_TO_AMOUNT_OF_PARAMS[opcode] + 1
-            print(i, instruction, sep=', ')
+            # print(i, instruction, sep=', ')
+
+    @staticmethod
+    def _get_opcode_from_instruction(instruction: str) -> int:
+        opcode_string = instruction[-2:]  # The opcode is based on the last two digits of the instruction.
+        return int(opcode_string)
 
     @classmethod
-    def _pad_modes(cls, instruction, opcode):
-        existing_modes = [int(mode) for mode in instruction[:-2]]
-        missing_modes_count = cls.OPCODES_TO_AMOUNT_OF_PARAMS[opcode] - len(existing_modes)
-        return list(existing_modes) + ([0] * missing_modes_count)
+    def _pad_modes(cls, instruction: str, opcode: int) -> list[int]:
+        """
+        :param instruction: instruction with maybe missing modes
+        :param opcode: opcode of the instruction
+        :return: instruction with padded zeroes that replace missing modes cv
+        """
+        amount_of_parameters = cls.OPCODES_TO_AMOUNT_OF_PARAMS[opcode]
+        existing_modes = instruction[:-2]
+        modes = existing_modes.rjust(amount_of_parameters, '0')
+        modes = reversed(modes)  # Modes are read from right to left, but indices start on the left.
+        return [int(mode) for mode in modes]
 
 
 if __name__ == '__main__':
