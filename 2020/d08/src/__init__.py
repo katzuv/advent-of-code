aa930b36eb13f20633567de176d9e2138418f253
Dan Katzuv <dan.dan.kafe@gmail.com>
2020-12-13 23:14:23 +0200

    Organize imports

---
+++ 2020/d08/src/__init__.py
@@ -0,0 +1,1 @@
+from .game_console import GameConsole
--- 2020/d08/src/operations/__init__.py
+++ 2020/d08/src/operations/__init__.py
@@ -1,1 +1,3 @@
-
+from .accumulator_operation import AccumulatorOperation
+from .jump_instruction import JumpOperation
+from .no_operation_instruction import NoOperationOperation
