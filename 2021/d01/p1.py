448c95682a29f2f53065c130791281dbf1e21b4b
Dan Katzuv <dan@katzuv.feigin>
2021-12-01 11:03:31 +0200

    Add function that gets measurements from the input text

--- 2021/d01/p1.py
+++ 2021/d01/p1.py
@@ -1,3 +1,11 @@
 from pathlib import Path
 
 INPUT_FILE_PATH = Path('..', 'inputs', '1.txt')
+
+
+def get_measurements_from_input(input_text: str) -> list:
+    """
+    :param input_text: input test to process
+    :return: split measurements from the input
+    """
+    return list(map(int, input_text.splitlines()))
