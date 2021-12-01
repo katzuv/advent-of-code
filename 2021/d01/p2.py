589c929b3b5649e6576219b7c1fd930288b2981e
Dan Katzuv <dan@katzuv.feigin>
2021-12-01 12:54:23 +0200

    Add main function and its execution

--- 2021/d01/p2.py
+++ 2021/d01/p2.py
@@ -1,5 +1,7 @@
 from typing import Iterator
 
+from p1 import INPUT_FILE_PATH, get_measurements_from_input, get_depth_measurements_increases
+
 _DEFAULT_WINDOW_SIZE = 3
 
 
@@ -16,3 +18,17 @@ def get_measurements_windows(measurements: Iterator[int], window_size: int = _DE
     for i in range(stop_index):
         windows.append(measurements[i:i + window_size])
     return windows
+
+
+def main():
+    input_text = INPUT_FILE_PATH.read_text()
+    measurements = get_measurements_from_input(input_text)
+    windows = get_measurements_windows(measurements)
+    windows_sums = [sum(window) for window in windows]
+
+    sums_increases = get_depth_measurements_increases(windows_sums)
+    print(f'Amount of measurements windows sums which are larger than the previous sum: {sums_increases}')
+
+
+if __name__ == '__main__':
+    main()
