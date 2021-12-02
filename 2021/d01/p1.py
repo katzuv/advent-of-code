64d1606c494510f68d23b151e66cc90ec2ae3fa1
Dan Katzuv <dan@katzuv.feigin>
2021-12-02 21:55:55 +0200

    Define the return type more specifically

--- 2021/d01/p1.py
+++ 2021/d01/p1.py
@@ -4,7 +4,7 @@ from typing import Iterator
 INPUT_FILE_PATH = Path('..', 'inputs', '1.txt')
 
 
-def get_measurements_from_input(input_text: str) -> list:
+def get_measurements_from_input(input_text: str) -> list[int]:
     """
     :param input_text: input test to process
     :return: split measurements from the input
