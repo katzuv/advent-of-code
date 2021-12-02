ba6e2844aabdbdec70c57c5fc65c2945632c2f58
Dan Katzuv <dan@katzuv.feigin>
2021-12-02 11:25:13 +0200

    Add constant holding step size key

--- 2021/d02/p1.py
+++ 2021/d02/p1.py
@@ -4,6 +4,7 @@ from pathlib import Path
 INPUT_FILE_PATH = Path('..', 'inputs', '2.txt')
 
 DIRECTION = 'direction'
+STEP_SIZE = 'step_size'
 
 
 Command = dict[str, int]  # Dictionary including direction and step size.
