5675ce9213858b70bae2c779193ba35d67f06718
Dan Katzuv <dan@katzuv.feigin>
2021-12-02 23:20:04 +0200

    Simplify windows generating solution with `zip`

--- 2021/d01/p2.py
+++ 2021/d01/p2.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Iterable
 
 from p1 import INPUT_FILE_PATH, get_measurements_from_input, get_depth_measurements_increases
 
@@ -6,7 +6,8 @@ from p1 import INPUT_FILE_PATH, get_measurements_from_input, get_depth_measureme
 _DEFAULT_WINDOW_SIZE = 3
 
 
-def get_measurements_windows(measurements: Iterator[int], window_size: int = _DEFAULT_WINDOW_SIZE) -> list[tuple[int]]:
+def get_measurements_windows(measurements: Iterator[int], window_size: int = _DEFAULT_WINDOW_SIZE) -> Iterable[
+                                                                                                      tuple[int]]:
     """
     Create sliding windows of measurements and return them.
     Stop when there are not enough measurements to produce a window with the given size.
@@ -14,11 +15,9 @@ def get_measurements_windows(measurements: Iterator[int], window_size: int = _DE
     :param window_size: size of every window
     :return: measurements windows
     """
-    windows = []
-    stop_index = (len(measurements) // window_size) * window_size
-    for i in range(stop_index):
-        windows.append(measurements[i:i + window_size])
-    return windows
+    measurements = list(measurements)
+    indented_measurements = (measurements[i:] for i in range(window_size))
+    return zip(*indented_measurements)
 
 
 def main():
