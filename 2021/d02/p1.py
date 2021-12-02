739aaa3816ef20d2ad74ceaba1217b0f05142c51
Dan Katzuv <dan@katzuv.feigin>
2021-12-02 11:33:08 +0200

    Add function that parses command from input text

--- 2021/d02/p1.py
+++ 2021/d02/p1.py
@@ -1,4 +1,5 @@
 import collections
+import re
 from pathlib import Path
 
 
@@ -9,3 +10,15 @@ STEP_SIZE = 'step_size'
 
 Command = collections.namedtuple('Command', (DIRECTION, STEP_SIZE))
 
+
+def get_commands_from_input(input_text: str) -> list[Command]:
+    """
+    :param input_text: input test to process
+    :return: commands from the input
+    """
+    commands = []
+    for command in input_text.splitlines():
+        match = re.match(fr'(?P<{DIRECTION}>forward|up|down) (?P<{STEP_SIZE}>\d+)', command)
+        direction, step_size = match.groupdict().values()
+        commands.append(Command(direction, int(step_size)))
+    return commands
