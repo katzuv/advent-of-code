9ae190e1022f92a54e2050452f15969b4cb92aa6
Dan Katzuv <dan@katzuv.feigin>
2021-12-04 00:47:56 +0200

    Add commands files paths constant

--- solution_runner/consts.py
+++ solution_runner/consts.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
+
 FIRST_AOC_YEAR = 2015
 
 _PYTHON_FILE_EXTENSION = '.py'
+COMMANDS_FILES_PATHS = tuple(Path('commands').glob(f'*{_PYTHON_FILE_EXTENSION}'))
 
 
 class CliConstants:
