48bdddba50d23aa97d4e68f3ab193aa682efb014
Dan Katzuv <dan@katzuv.feigin>
2021-12-04 00:46:40 +0200

    Add Python file extension

--- solution_runner/consts.py
+++ solution_runner/consts.py
@@ -1,10 +1,6 @@
-from pathlib import Path
-
-
 FIRST_AOC_YEAR = 2015
 
 _PYTHON_FILE_EXTENSION = '.py'
-COMMANDS_FILES_PATHS = Path('commands').glob(f'*{_PYTHON_FILE_EXTENSION}')
 
 
 class CliConstants:
