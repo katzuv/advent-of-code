d07aaade34d31b185041f16728432c653e216f0f
Dan Katzuv <dan@katzuv.feigin>
2021-12-04 01:50:46 +0200

    Use filename stem only instead of whole filename

--- solution_runner/modules_utils.py
+++ solution_runner/modules_utils.py
@@ -12,7 +12,7 @@ def get_module_from_filepath(filepath: Path, directory_relative_to: Path | str =
     :param directory_relative_to: directory to compute relative path to from `file_path`, default is current directory
     :return: imported module from `file_path`
     """
-    module_name = str(filepath.relative_to(directory_relative_to))
+    module_name = filepath.relative_to(directory_relative_to).stem
     for path_form, module_form in consts.SEPARATORS_TO_MODULE_NOTATION.items():
         module_name = module_name.replace(path_form, module_form)
     return importlib.import_module(module_name)
