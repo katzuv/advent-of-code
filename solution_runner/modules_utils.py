06572e772309f732bac3503496c21c2a19906ab1
Dan Katzuv <dan@katzuv.feigin>
2021-12-04 01:45:13 +0200

    Add function that imports a module from a filepath

--- solution_runner/modules_utils.py
+++ solution_runner/modules_utils.py
@@ -1,16 +1,18 @@
 """Filepaths and modules utils."""
+import importlib
 from pathlib import Path
+from types import ModuleType
 
 import consts
 
 
-def convert_filepath_to_relative_module(filepath: Path, directory_relative_to: Path | str = Path('.')) -> str:
+def get_module_from_filepath(filepath: Path, directory_relative_to: Path | str = Path('.')) -> ModuleType:
     """
-    :param filepath: file path to convert to module
+    :param filepath: file path to import
     :param directory_relative_to: directory to compute relative path to from `file_path`, default is current directory
-    :return: module name in relative terms to `file_path`
+    :return: imported module from `file_path`
     """
     module_name = str(filepath.relative_to(directory_relative_to))
-    for path_form, module_form in consts.SEPARATORS_TO_MODULE_NOTATION.values():
+    for path_form, module_form in consts.SEPARATORS_TO_MODULE_NOTATION.items():
         module_name = module_name.replace(path_form, module_form)
-    return module_name
+    return importlib.import_module(module_name)
