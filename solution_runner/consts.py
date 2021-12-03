024436ef066b9ff8658463acf539f9c99aa1fe73
Dan Katzuv <dan@katzuv.feigin>
2021-12-04 01:18:39 +0200

    Add separators to module notation

--- solution_runner/consts.py
+++ solution_runner/consts.py
@@ -5,6 +5,8 @@ FIRST_AOC_YEAR = 2015
 
 _PYTHON_FILE_EXTENSION = '.py'
 COMMANDS_FILES_PATHS = tuple(Path('commands').glob(f'*{_PYTHON_FILE_EXTENSION}'))
+# Linux and Windows separators to module notation, including upper package and accessing module inside package.
+SEPARATORS_TO_MODULE_NOTATION = {'../': '..', '/': '.', '..\\': '..', '\\': '.'}
 
 
 class CliConstants:
