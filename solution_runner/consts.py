45802a6d37c4774ef2aa9568fa5e9e191c58cf4a
Dan Katzuv <dan@katzuv.feigin>
2021-12-04 12:08:51 +0200

    Change commands files globbing to accept special suffix only
    This is to ignore other non-command Python files, such as constants file.

--- solution_runner/consts.py
+++ solution_runner/consts.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 
-_PYTHON_FILE_EXTENSION = '.py'
-COMMANDS_FILES_PATHS = tuple(Path('commands').glob(f'*{_PYTHON_FILE_EXTENSION}'))
+COMMAND_FILE_SUFFIX = '_command.py'
+COMMANDS_FILES_PATHS = tuple(Path('commands').glob(f'*{COMMAND_FILE_SUFFIX}'))
 # Linux and Windows separators to module notation, including upper package and accessing module inside package.
 SEPARATORS_TO_MODULE_NOTATION = {'../': '..', '/': '.', '..\\': '..', '\\': '.'}
 
