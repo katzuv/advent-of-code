2e11b7f1c5f6283d06070e46c05aae40ea35b0d4
Dan Katzuv <dan@katzuv.feigin>
2021-12-08 17:17:08 +0200

    Move setup command to another file

--- solution_runner/cli.py
+++ solution_runner/cli.py
@@ -2,7 +2,6 @@ import click
 
 import consts
 from consts import CliConstants
-from defaults_and_choices import get_default_year
 
 
 @click.group(context_settings=CliConstants.CONTEXT)
@@ -12,15 +11,6 @@ def cli() -> click.Group:
 
     For more information about Advent of Code, see https://adventofcode.com.
     """
-
-
-@cli.command()
-@click.option('-y', '--year', type=click.IntRange(consts.FIRST_AOC_YEAR, get_default_year()),
-              default=get_default_year(), help='year of challenge setting up solution for')
-@click.option('-d', '--day', type=click.IntRange(1, 25), help='day of challenge setting up solution for')
-@click.option('--use_cache/--ignore_cache', default=True, help='whether to use cached input file [default: true]')
-def setup(year: int, day: int, use_cache: bool):
-    """Set up a solution: fetch input and create solution files."""
 
 
 if __name__ == '__main__':
---
+++ solution_runner/commands/setup_command.py
@@ -0,0 +1,13 @@
+import click
+
+import consts
+from defaults_and_choices import get_default_year
+
+
+@click.command(name='setup')
+@click.option('-y', '--year', type=click.IntRange(consts.FIRST_AOC_YEAR, get_default_year()),
+              default=get_default_year(), help='year of challenge setting up solution for')
+@click.option('-d', '--day', type=click.IntRange(1, 25), help='day of challenge setting up solution for')
+@click.option('--use_cache/--ignore_cache', default=True, help='whether to use cached input file [default: true]')
+def command(year: int, day: int, use_cache: bool):
+    """Set up a solution: fetch input and create solution files."""
