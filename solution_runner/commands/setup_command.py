a7af43e19f1b7043ad91225bcd5a63250369a7c0
Dan Katzuv <dan@katzuv.feigin>
2021-12-08 17:36:57 +0200

    Add "should" to "use_cache" to clarify it's a boolean

--- solution_runner/commands/setup_command.py
+++ solution_runner/commands/setup_command.py
@@ -9,5 +9,5 @@ from defaults_and_choices import get_default_year
               default=get_default_year(), help='year of challenge setting up solution for')
 @click.option('-d', '--day', type=click.IntRange(1, 25), help='day of challenge setting up solution for')
 @click.option('--use_cache/--ignore_cache', default=True, help='whether to use cached input file [default: true]')
-def command(year: int, day: int, use_cache: bool):
+def command(year: int, day: int, should_use_cache: bool):
     """Set up a solution: fetch input and create solution files."""
