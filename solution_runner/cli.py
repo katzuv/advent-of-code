bba1781750c3e6df4efa34e6ef9f89ba5c8d7316
Dan Katzuv <dan@katzuv.feigin>
2021-12-08 17:22:42 +0200

    Add function that import a subcommand from a module

--- solution_runner/cli.py
+++ solution_runner/cli.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+from typing import Iterator
+
 import click
 
 import consts
 from consts import CliConstants
+from solution_runner.modules_utils import get_module_from_filepath
 
 
 @click.group(context_settings=CliConstants.CONTEXT)
@@ -11,6 +15,17 @@ def cli() -> click.Group:
 
     For more information about Advent of Code, see https://adventofcode.com.
     """
+
+
+def _add_commands_to_cli(cli: click.Group, commands_files_paths: Iterator[Path]) -> click.Group:
+    """
+    :param cli: click.Group object
+    :param commands_files_paths: path to directory containing Python files with Python subcommands
+    :return: CLI with added subcommands
+    """
+    for filepath in commands_files_paths:
+        module = get_module_from_filepath(filepath)
+        cli.add_command(module.command)
 
 
 if __name__ == '__main__':
