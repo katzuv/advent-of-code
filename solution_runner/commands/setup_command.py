afe02681dcec8dacd7b226428337474c2fd663a1
Dan Katzuv <dan@katzuv.feigin>
2021-12-10 12:26:19 +0200

    Add parameter name of use cache option

--- solution_runner/commands/setup_command.py
+++ solution_runner/commands/setup_command.py
@@ -8,7 +8,8 @@ from defaults_and_choices import get_default_year
 @click.option('-y', '--year', type=click.IntRange(consts.FIRST_AOC_YEAR, get_default_year()),
               default=get_default_year(), help='year of challenge setting up solution for')
 @click.option('-d', '--day', type=click.IntRange(1, 25), required=True, help='day of challenge setting up solution for')
-@click.option('--use_cache/--ignore_cache', default=True, help='whether to use cached input file [default: true]')
+@click.option('--use_cache/--ignore_cache', 'should_use_cache', default=True,
+              help='whether to use cached input file [default: true]')
 @click.option('--session_id', envvar='AOC_SESSION_ID', prompt=True, help='session ID to access challenges input')
 def command(year: int, day: int, should_use_cache: bool, session_id: str):
     """Set up a solution: fetch input and create solution files."""
