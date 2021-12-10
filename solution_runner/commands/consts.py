e3d752a875096b6ae42b1cd968efcc067073146f
Dan Katzuv <dan@katzuv.feigin>
2021-12-10 23:56:03 +0200

    Add constant holding US/Eastern timezone

--- solution_runner/commands/consts.py
+++ solution_runner/commands/consts.py
@@ -1,6 +1,10 @@
+import datetime
 FIRST_AOC_YEAR = 2015
 ZERO = '0'
 
 
 class FileExtensions:
     PYTHON = '.py'
+
+
+AOC_UNLOCK_TIME_TEMPLATE = datetime.datetime(year=1, month=12, day=1, hour=0, tzinfo=US_EASTERN_TIMEZONE)
