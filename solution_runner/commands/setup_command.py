a776f90fa81f95ce4090c45cd95cde8af45885cb
Dan Katzuv <dan@katzuv.feigin>
2021-12-10 22:47:53 +0200

    Add prompt if user didn't enter root directory

--- solution_runner/commands/setup_command.py
+++ solution_runner/commands/setup_command.py
@@ -10,7 +10,7 @@ from defaults_and_choices import get_default_year
 @click.option('-d', '--day', type=click.IntRange(1, 25), required=True, help='day of challenge setting up solution for')
 @click.option('--use_cache/--ignore_cache', 'should_use_cache', default=True,
               help='whether to use cached input file [default: true]')
-@click.option('--root', 'root_directory', envvar='AOC_ROOT_DIRECTORY',
+@click.option('--root', 'root_directory', envvar='AOC_ROOT_DIRECTORY', prompt=True,
               type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True, path_type=Path,
                               resolve_path=True), help='root directory of Advent of Code challenges solutions')
 @click.option('--session_id', envvar='AOC_SESSION_ID', prompt=True,
