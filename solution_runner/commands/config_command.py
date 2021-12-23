61e974a80abdf88e4e9d2bd78acbdc99516fd82a
Dan Katzuv <dan@katzuv.feigin>
2021-12-23 18:14:47 +0200

    Add session ID parameter

--- solution_runner/commands/config_command.py
+++ solution_runner/commands/config_command.py
@@ -7,6 +7,7 @@ import click
 @click.option('--root', 'root_directory',
               type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True, path_type=Path,
                               resolve_path=True), help='root directory of Advent of Code puzzles project')
-def command(root_directory: Path):
+@click.option('--session-id', help='session ID to access puzzles input')
+def command(root_directory: Path, session_id: str):
     """Set options."""
     pass
