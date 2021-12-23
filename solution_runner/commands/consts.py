eb71e53260b30a8ae078d009ee07890b2e3d1cb1
Dan Katzuv <dan@katzuv.feigin>
2021-12-23 17:58:04 +0200

    Add import for unlock time template

--- solution_runner/commands/consts.py
+++ solution_runner/commands/consts.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 from zoneinfo import ZoneInfo
 
@@ -19,4 +20,4 @@ class FileExtensions:
 
 
 US_EASTERN_TIMEZONE = ZoneInfo('US/Eastern')
-AOC_UNLOCK_TIME_TEMPLATE = datetime.datetime(year=1, month=12, day=1, hour=0, tzinfo=US_EASTERN_TIMEZONE)
+AOC_UNLOCK_TIME_TEMPLATE = datetime(year=1, month=12, day=1, hour=0, tzinfo=US_EASTERN_TIMEZONE)
