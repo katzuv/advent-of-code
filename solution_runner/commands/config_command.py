afa4c527c0456ad3c5d17ae71dedfc5c2143c55c
Dan Katzuv <dan@katzuv.feigin>
2021-12-23 18:14:07 +0200

    Add root directory parameter

--- solution_runner/commands/config_command.py
+++ solution_runner/commands/config_command.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
 import click
 
 
 @click.command(name='config')
-def command():
+@click.option('--root', 'root_directory',
+              type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True, path_type=Path,
+                              resolve_path=True), help='root directory of Advent of Code puzzles project')
+def command(root_directory: Path):
     """Set options."""
     pass
