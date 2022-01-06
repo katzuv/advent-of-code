52ca64fc3665ad27ec862ee3451fafb0a2336cbf
Dan Katzuv <dan@katzuv.feigin>
2022-01-06 22:37:31 +0200

    Read from configuration file and create it and its directory if needed

--- solution_runner/commands/config_command.py
+++ solution_runner/commands/config_command.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import click
+import yaml
 
 import consts
 
@@ -13,3 +14,10 @@ def command(root_directory: str, session_id: str):
     """Set options."""
     app_data_directory = click.get_app_dir(consts.APP_DATA_DIRECTORY)
     configuration_file = Path(app_data_directory, consts.CONFIGURATION_FILE_NAME)
+    try:
+        # Use an empty dictionary if no actual configuration is in the configuration file.
+        configuration = yaml.safe_load(configuration_file.read_text()) or {}
+    except FileNotFoundError:
+        Path(app_data_directory).mkdir(exist_ok=True)
+        configuration_file.touch()
+        configuration = {}
