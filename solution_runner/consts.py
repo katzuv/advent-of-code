cf536d2c2bc62a7bd0304889af5d91c5a9740098
Dan Katzuv <dan@katzuv.feigin>
2022-01-07 00:17:27 +0200

    Create command file lists from absolute path

--- solution_runner/consts.py
+++ solution_runner/consts.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 
 COMMAND_FILE_SUFFIX = '_command.py'
-COMMANDS_FILES_PATHS = tuple(Path('commands').glob(f'*{COMMAND_FILE_SUFFIX}'))
+_COMMANDS_DIRECTORY_PATH = Path(__file__).parent / Path('commands')
+COMMANDS_FILES_PATHS = tuple(_COMMANDS_DIRECTORY_PATH.glob(f'*{COMMAND_FILE_SUFFIX}'))
 # Linux and Windows separators to module notation, including upper package and accessing module inside package.
 SEPARATORS_TO_MODULE_NOTATION = {'../': '..', '/': '.', '..\\': '..', '\\': '.'}
 
