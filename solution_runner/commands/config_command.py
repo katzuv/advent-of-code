db9e7bdfcdab9b0e405484eadd2f34f6ee9ca77d
Dan Katzuv <dan@katzuv.feigin>
2022-01-06 22:36:12 +0200

    Create configuration file variable

--- solution_runner/commands/config_command.py
+++ solution_runner/commands/config_command.py
@@ -12,3 +12,4 @@ import consts
 def command(root_directory: str, session_id: str):
     """Set options."""
     app_data_directory = click.get_app_dir(consts.APP_DATA_DIRECTORY)
+    configuration_file = Path(app_data_directory, consts.CONFIGURATION_FILE_NAME)
