6c1ecd34c5555e7cc80cf7928e0f1dfd3ae09bea
Dan Katzuv <dan@katzuv.feigin>
2022-01-06 22:57:41 +0200

    Add hidden prompt for session ID

--- solution_runner/commands/config_command.py
+++ solution_runner/commands/config_command.py
@@ -10,7 +10,8 @@ import consts
 @click.command(name='config')
 @click.option('--root', 'root_directory', type=consts.ROOT_DIRECTORY_TYPE, prompt=True, prompt_required=False,
               help='root directory of Advent of Code puzzles project')
-@click.option('--session-id', help='session ID to access puzzles input')
+@click.option('--session-id', prompt=True, prompt_required=False, hide_input=True,
+              help='session ID to access puzzles input')
 def command(root_directory: str, session_id: str):
     """Set options."""
     app_data_directory = click.get_app_dir(consts.APP_DATA_DIRECTORY)
