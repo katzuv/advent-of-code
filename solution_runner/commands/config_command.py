20864edcaee87d58f67189fef639e498099428c4
Dan Katzuv <dan@katzuv.feigin>
2022-01-06 21:57:53 +0200

    Change root directory parameter type to new constant

--- solution_runner/commands/config_command.py
+++ solution_runner/commands/config_command.py
@@ -4,10 +4,9 @@ import click
 
 
 @click.command(name='config')
-@click.option('--root', 'root_directory',
-              type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True, path_type=Path,
-                              resolve_path=True), help='root directory of Advent of Code puzzles project')
+@click.option('--root', 'root_directory', type=consts.ROOT_DIRECTORY_TYPE, prompt=True, prompt_required=False,
+              help='root directory of Advent of Code puzzles project')
 @click.option('--session-id', help='session ID to access puzzles input')
-def command(root_directory: Path, session_id: str):
+def command(root_directory: str, session_id: str):
     """Set options."""
     pass
