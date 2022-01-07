3f1344a83fe763fc4f4f959197c8422620cd187f
Dan Katzuv <dan@katzuv.feigin>
2022-01-07 17:47:20 +0200

    Create function that returns a given key's configured value

--- solution_runner/utils.py
+++ solution_runner/utils.py
@@ -1,7 +1,12 @@
 import importlib
 from pathlib import Path
 from types import ModuleType
+from typing import Any
 
+import click
+import yaml
+
+import commands.consts
 import consts
 
 
@@ -15,3 +20,18 @@ def get_module_from_filepath(filepath: Path, directory_relative_to: Path | str =
     for path_form, module_form in consts.SEPARATORS_TO_MODULE_NOTATION.items():
         module_name = module_name.replace(path_form, module_form)
     return importlib.import_module(module_name)
+
+
+def get_setting(key: str) -> Any:
+    """
+    :param key: key to retrieve its value
+    :return: value of `key` which is stored in the configuration file
+    """
+    configuration_file = commands.consts.APP_DATA_DIRECTORY / commands.consts.CONFIGURATION_FILE_NAME
+    try:
+        configuration = yaml.safe_load(configuration_file.read_text())
+    except FileNotFoundError:
+        click.secho("configuration file doesn't exist. Run config command first", fg='red')
+        raise
+
+    return configuration[key]
