f09a1b3133ef4ef67da871078c2f197376816173
Dan Katzuv <dan@katzuv.feigin>
2022-02-11 20:12:56 +0200

    Cast `directory_relative_to` to `Path` if it is an `str`

--- solution_runner/utils.py
+++ solution_runner/utils.py
@@ -11,6 +11,8 @@ def get_module_from_filepath(filepath: Path, directory_relative_to: Path | str =
     :param directory_relative_to: directory to compute relative path to from `file_path`, default is current directory
     :return: imported module from `file_path`
     """
+    if isinstance(directory_relative_to, str):
+        directory_relative_to = Path(directory_relative_to)
     module_name = str(filepath.relative_to(directory_relative_to.absolute()).with_suffix(''))
     for path_form, module_form in consts.SEPARATORS_TO_MODULE_NOTATION.items():
         module_name = module_name.replace(path_form, module_form)
