167a9226fff4876fd237494e8e2dad12fc038fe2
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-10-16 20:40:13 +0300

    Remove dynamic commands loading code
    Too complex and not worth it for a single digit number of commands.

--- solution_runner/cli.py
+++ solution_runner/cli.py
@@ -1,12 +1,6 @@
-import os
-from pathlib import Path
-from typing import Iterator
-
 import click
 
-import consts
-from consts import CliConstants
-from utils import get_module_from_filepath
+from solution_runner.consts import CliConstants
 
 
 @click.group(context_settings=CliConstants.CONTEXT)
@@ -18,24 +12,8 @@ def cli() -> click.Group:
     """
 
 
-def _add_commands_to_cli(cli: click.Group, commands_files_paths: Iterator[Path]) -> click.Group:
-    """
-    :param cli: click.Group object
-    :param commands_files_paths: path to directory containing Python files with Python subcommands
-    :return: CLI with added subcommands
-    """
-    file_directory = Path(__file__).parent
-    current_directory = Path.cwd()
-    os.chdir(file_directory)
-    for filepath in commands_files_paths:
-        module = get_module_from_filepath(filepath)
-        cli.add_command(module.command)
-    os.chdir(current_directory)
-
-
 def main():
     """Add subcommands to the solution runner CLI and run it."""
-    _add_commands_to_cli(cli, consts.COMMANDS_FILES_PATHS)
     cli()
 
 
--- solution_runner/consts.py
+++ solution_runner/consts.py
@@ -1,13 +1,2 @@
-import os
-from pathlib import Path
-
-
-COMMAND_FILE_SUFFIX = '_command.py'
-_COMMANDS_DIRECTORY_PATH = Path(__file__).parent / Path('commands')
-COMMANDS_FILES_PATHS = tuple(_COMMANDS_DIRECTORY_PATH.glob(f'*{COMMAND_FILE_SUFFIX}'))
-# Separators to module notation, including upper package and accessing module inside package.
-SEPARATORS_TO_MODULE_NOTATION = {f'..{os.sep}': '..', os.sep: '.'}
-
-
 class CliConstants:
     CONTEXT = {'help_option_names': ['-h', '--help']}
