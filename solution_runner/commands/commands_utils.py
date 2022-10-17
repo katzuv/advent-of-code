186525878ae4d4cccbd1c4d50c2800008e8753d4
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-10-18 00:09:52 +0300

    Add function that checks whether a path exists and aborts if it does not

--- solution_runner/commands/commands_utils.py
+++ solution_runner/commands/commands_utils.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from pathlib import Path
 from typing import Any
 
 import click
@@ -25,3 +26,25 @@ def get_setting(key: str) -> Any:
         raise
 
     return configuration[key]
+
+
+def check_path_exists(path: Path, command: click.Command, path_type: str = None):
+    """
+    Abort if path doesn't exist.
+    :param path: path to check
+    :param command: command to be passed to`click.Context` if aborting is needed
+    :param path_type: optional "dir" or "file". If not passed, only check for existence. If passed, check for type.
+    """
+    match path_type:
+        case None:
+            if not path.exists():
+                click.secho(f"{path} doesn't exist", fg='red')
+        case "dir":
+            if not path.is_dir():
+                click.secho(f"Directory at {path} doesn't exist or isn't a directory", fg='red')
+        case "file":
+            if not path.is_file():
+                click.secho(f"File at {path} doesn't exist or isn't a file", fg='red')
+        case _:
+            return  # If everything is OK, exit the function before aborting.
+    click.Context(command).abort()
