2cecc694d267b16ff4cb76fe04f6c2757cb1ac89
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-10-30 22:48:25 +0200

    Add part option to `submit` command

--- solution_runner/commands/submit_command.py
+++ solution_runner/commands/submit_command.py
@@ -11,6 +11,7 @@ _default_year = get_default_year()
 @click.option('-y', '--year', type=click.IntRange(consts.FIRST_AOC_YEAR, _default_year), default=_default_year,
               show_default=f'last year: {_default_year}', help='year of puzzle setting up solution for')
 @click.option('-d', '--day', type=consts.ADVENT_DAYS_RANGE, required=True, help='day of puzzle to submit')
-def command(year: int, day: int):
+@click.option('-p', '--part', type=click.Choice(('1', '2')), required=True, help='puzzle part to submit')
+def command(year: int, day: int, part: int):
     """Submit solution."""
     pass
