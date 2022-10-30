ced68d4276f56562744e62463f25eaa94b2299f6
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-10-30 22:46:25 +0200

    Add year option to `submit` command

--- solution_runner/commands/submit_command.py
+++ solution_runner/commands/submit_command.py
@@ -1,7 +1,15 @@
 import click
 
+from . import consts
+from .defaults_and_choices import get_default_year
+
+
+_default_year = get_default_year()
+
 
 @click.command(name='submit')
-def command():
+@click.option('-y', '--year', type=click.IntRange(consts.FIRST_AOC_YEAR, _default_year), default=_default_year,
+              show_default=f'last year: {_default_year}', help='year of puzzle setting up solution for')
+def command(year: int):
     """Submit solution."""
     pass
