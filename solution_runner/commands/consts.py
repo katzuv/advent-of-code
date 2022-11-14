177435132932a9ee04fbaf7536fe049ec997857e
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-11-15 00:17:35 +0200

    Add submit endpoint template

--- solution_runner/commands/consts.py
+++ solution_runner/commands/consts.py
@@ -12,6 +12,7 @@ ADVENT_DAYS_RANGE = click.IntRange(1, 25)
 ZERO = '0'
 BASE_URL = 'https://adventofcode.com/'
 INPUT_ENDPOINT_TEMPLATE = string.Template('/$year/day/$day/input')
+SUBMIT_ENDPOINT_TEMPLATE = string.Template('/$year/day/$day/answer')
 SESSION = 'session'
 # Requested by Advent of Code owner to help track requests.
 USER_AGENT_HEADER = {'User-Agent': 'AoC.CLI.katzuv'}
