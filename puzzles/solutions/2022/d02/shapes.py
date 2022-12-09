1441ba330c8e19e9fdd09628cfecb99d2db9d754
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 01:09:20 +0200

    Add constants for the shapes

--- puzzles/solutions/2022/d02/shapes.py
+++ puzzles/solutions/2022/d02/shapes.py
@@ -1,1 +1,9 @@
 """Various constants used for Rock Paper Scissors shapes and other data about them."""
+
+
+OUR_ROCK = "X"
+OUR_PAPER = "Y"
+OUR_SCISSORS = "Z"
+OPPONENT_ROCK = "A"
+OPPONENT_PAPER = "B"
+OPPONENT_SCISSORS = "C"
