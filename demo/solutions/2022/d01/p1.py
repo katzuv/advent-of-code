8c53c512d36ae52bd02cc6610c3962dcb2a7c3a4
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-09 21:31:51 +0200

    Add function that parses the input

--- demo/solutions/2022/d01/p1.py
+++ demo/solutions/2022/d01/p1.py
@@ -1,6 +1,15 @@
 import sys
 
 
+def get_inventories(input_text: str) -> list[list[int]]:
+    """
+    :param input_text: puzzle input
+    :return: list of elves' inventories
+    """
+    inventories = input_text.split("\n\n")  # Inventories are split by a blank line.
+    return [[int(item) for item in inventory.splitlines()] for inventory in inventories]
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
