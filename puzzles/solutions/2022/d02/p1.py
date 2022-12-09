717483cf76a050dd3e517ed32e1077c36242d9ac
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 00:39:49 +0200

    Add function that parses the input

--- puzzles/solutions/2022/d02/p1.py
+++ puzzles/solutions/2022/d02/p1.py
@@ -1,6 +1,15 @@
 import sys
 
 
+def get_moves(input_text: str) -> list[tuple[str, str]]:
+    """
+    :param input_text: puzzle input
+    :return: list of (opponent move, our move) couples
+    """
+    moves = input_text.splitlines()
+    return [tuple(move.split()) for move in moves]
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
