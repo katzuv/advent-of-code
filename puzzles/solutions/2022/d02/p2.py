30d36784acc7240807c63e906f19e81dbcc0bb59
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 01:45:57 +0200

    Add function that returns what shape should be played

--- puzzles/solutions/2022/d02/p2.py
+++ puzzles/solutions/2022/d02/p2.py
@@ -1,9 +1,29 @@
 import sys
 
+import shapes
+
 
 DEFEAT = "X"
 DRAW = "Y"
 WIN = "Z"
+
+
+def choose_shape(opponent_move: str, outcome: str) -> str:
+    """
+    :param opponent_move: move the opponent played
+    :param outcome: desired outcome of the move
+    :return: shape to play for the desired outcome to happen
+    """
+    if outcome == DEFEAT:
+        return shapes.OPPONENT_TO_OUR_SHAPE_DEFEAT[opponent_move]
+    if outcome == DRAW:
+        for our_shape, opponent_shape in shapes.IDENTICAL_SHAPES.items():
+            if opponent_shape == opponent_move:
+                return our_shape
+    if outcome == WIN:
+        for our_shape, opponent_shape in shapes.OUR_TO_OPPONENT_SHAPE_DEFEAT.items():
+            if opponent_shape == opponent_move:
+                return our_shape
 
 
 def get_answer(input_text: str):
