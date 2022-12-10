bc23862a28eacbea9c95194c435bbfefb746abc6
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 02:17:35 +0200

    Add function that parses the input

--- puzzles/solutions/2022/d03/p1.py
+++ puzzles/solutions/2022/d03/p1.py
@@ -1,5 +1,20 @@
 import sys
 
+def get_rucksacks_compartments(
+    input_text: str,
+) -> list[tuple[set, set]]:
+    """
+    :param input_text: puzzle input
+    :return: list of (first compartment set, second compartment set) couples
+    """
+    return [
+        (
+            set(rucksack[: len(rucksack) // 2]),
+            set(rucksack[len(rucksack) // 2 :]),
+        )
+        for rucksack in input_text.splitlines()
+    ]
+
 
 def get_answer(input_text: str):
     raise NotImplementedError
