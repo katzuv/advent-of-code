ed1911fa06f0308ccde36cd9dd853c2b28164170
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 14:10:03 +0200

    Remove "our" from shapes names

--- puzzles/solutions/2022/d02/shapes.py
+++ puzzles/solutions/2022/d02/shapes.py
@@ -1,9 +1,9 @@
 """Various constants used for Rock Paper Scissors shapes and other data about them."""
 
 
-OUR_ROCK = "X"
-OUR_PAPER = "Y"
-OUR_SCISSORS = "Z"
+ROCK = "X"
+PAPER = "Y"
+SCISSORS = "Z"
 OPPONENT_ROCK = "A"
 OPPONENT_PAPER = "B"
 OPPONENT_SCISSORS = "C"
