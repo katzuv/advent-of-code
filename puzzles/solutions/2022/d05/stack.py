6dcc6f008fcfca3a2fbb1462110b45ffa054c506
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 22:42:38 +0200

    Create `Stack` class constructor

--- puzzles/solutions/2022/d05/stack.py
+++ puzzles/solutions/2022/d05/stack.py
@@ -1,2 +1,12 @@
+from typing import Sequence
+
+
 class Stack:
     """Class representing a stack of crates."""
+
+    def __init__(self, initial_crates: Sequence[str]):
+        """
+        Instantiate a stack.
+        :param initial_crates: initial crates in the stack
+        """
+        self._crates = list(initial_crates)
