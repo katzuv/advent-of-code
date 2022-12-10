c21a7c37066a850549c59d006bbfe02f1317143f
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 22:49:40 +0200

    Add method that removes crates from a `Stack` and returns them

--- puzzles/solutions/2022/d05/stack.py
+++ puzzles/solutions/2022/d05/stack.py
@@ -10,3 +10,13 @@ class Stack:
         :param initial_crates: initial crates in the stack
         """
         self._crates = list(initial_crates)
+
+    def remove_crates(self, amount: int) -> list[str]:
+        """
+        Remove crates from the top of the stack and return a list of them.
+        :param amount: amount of crates to remove
+        :return: removed crates
+        """
+        removed = self._crates[-amount:]
+        self._crates = self._crates[:-amount]
+        return removed
