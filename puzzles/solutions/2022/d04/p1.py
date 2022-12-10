702a143016bf63acd92e3ba9416cc26cc6b25bdf
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 22:12:06 +0200

    Add function that parses section IDs from the input

--- puzzles/solutions/2022/d04/p1.py
+++ puzzles/solutions/2022/d04/p1.py
@@ -1,6 +1,23 @@
 import sys
 
 
+def get_sections(input_text: str) -> tuple[tuple[set[int], set[int]]]:
+    """
+    :param input_text: puzzle input
+    :return: tuple of (first section IDs, second section IDs) pairs
+    """
+    pairs = input_text.splitlines()
+    sections = []
+    for pair in pairs:
+        pair_sections = []
+        for section_range in pair.split(","):
+            start, end = map(int, section_range.split("-"))
+            section_ids = set(range(start, end + 1))
+            pair_sections.append(section_ids)
+        sections.append(tuple(pair_sections))
+    return tuple(sections)
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
