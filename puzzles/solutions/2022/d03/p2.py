5efbb08edab953ebb4141d30fad8a912ecbe5a9d
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 02:47:02 +0200

    Add function that parses the input

--- puzzles/solutions/2022/d03/p2.py
+++ puzzles/solutions/2022/d03/p2.py
@@ -1,5 +1,20 @@
 import sys
 
+GROUP_SIZE = 3
+
+
+def get_group_rucksacks(input_text: str) -> list[list[str]]:
+    """
+    :param input_text: puzzle input
+    :return: list of combined rucksacks in a group
+    """
+    rucksacks = input_text.splitlines()
+    groups = []
+    for rucksack_number in range(0, len(rucksacks), GROUP_SIZE):
+        group_rucksacks = rucksacks[rucksack_number : rucksack_number + GROUP_SIZE]
+        groups.append(group_rucksacks)
+    return groups
+
 
 def get_answer(input_text: str):
     raise NotImplementedError
