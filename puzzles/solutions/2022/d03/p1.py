ae9af8bc6e645cd34488e3674a93d299edec3569
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-10 02:23:39 +0200

    Add function that returns the shared item type between two compartments

--- puzzles/solutions/2022/d03/p1.py
+++ puzzles/solutions/2022/d03/p1.py
@@ -16,6 +16,15 @@ def get_rucksacks_compartments(
     ]
 
 
+def get_shared_item_type(first_compartment: set, second_compartment: set) -> str:
+    """
+    :param first_compartment: counter of item types in the first compartment
+    :param second_compartment: counter of item types in the second compartment
+    :return: shared item type between the two compartments
+    """
+    return (first_compartment & second_compartment).pop()
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
