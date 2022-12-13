c8e0a91451f6e170a502fedc26ddba8e456ac34b
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-14 01:25:41 +0200

    Create function that returns index of the first start-of-message marker

--- puzzles/solutions/2022/d06/p2.py
+++ puzzles/solutions/2022/d06/p2.py
@@ -1,12 +1,14 @@
 import sys
 
+import p1
+
 
 START_OF_MESSAGE_MINIMUM_UNIQUE_SEQUENCE_LENGTH = 14
 
 
 def get_answer(input_text: str):
-    raise NotImplementedError
-
+    """Return how many characters need to be processed before the first start-of-message marker is detected."""
+    return p1.get_unique_sequence_start_index(input_text, START_OF_MESSAGE_MINIMUM_UNIQUE_SEQUENCE_LENGTH)
 
 if __name__ == "__main__":
     try:
