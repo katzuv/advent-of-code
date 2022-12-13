5b91508c8a9d363ccd833187d33ca4d90fa793ad
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-14 00:39:19 +0200

    Add function that executes a crates transfer step
    Now when crates retain their order.

--- puzzles/solutions/2022/d05/p2.py
+++ puzzles/solutions/2022/d05/p2.py
@@ -1,4 +1,20 @@
 import sys
+from typing import Sequence
+
+from stack import Stack
+from step import Step
+
+
+def execute_transfer_step(stacks: Sequence[Stack], step: Step):
+    """
+    Process a transferring of crates between stacks.
+    :param stacks: sequence of `Stack`s
+    :param step: step to execute
+    """
+    crates = stacks[step.src - 1].remove_crates(step.amount)
+    stacks[step.dst - 1].add_crates(
+        reversed(tuple(crates))
+    )  # Reverse the crates because now they now retain their order when transferred.
 
 
 def get_answer(input_text: str):
