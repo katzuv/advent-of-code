940c6a6ebc0390ebcd8db719c425b8ef52fcd653
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-14 17:56:23 +0200

    Add constructor to `Directory` class

--- puzzles/solutions/2022/d07/directory.py
+++ puzzles/solutions/2022/d07/directory.py
@@ -1,2 +1,16 @@
+from typing import Self
+
+
 class Directory:
     """A directory which can store files and subdirectories."""
+
+    def __init__(self, name: str, parent: Self = None):
+        """
+        Instantiate a directory.
+        :param name: name of the directory
+        :param parent: parent directory, if exists
+        """
+        self.name = name
+        self.parent = parent
+        self._files = []
+        self._subdirectories = []
