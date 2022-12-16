31d0a69823f56869c974daaecd8c0e75ed71e998
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-16 11:23:29 +0200

    Add function that returns root directory after parsing terminal output

--- puzzles/solutions/2022/d07/p1.py
+++ puzzles/solutions/2022/d07/p1.py
@@ -1,4 +1,28 @@
 import sys
+from typing import Sequence
+
+import consts
+import input_parsing
+from directory import Directory
+
+
+def build_filesystem(commands: Sequence[str]) -> Directory:
+    """
+    Build the filesystem according to the given commands and return the root directory.
+    :param commands: commands output
+    :return: root directory
+    """
+    root = Directory(consts.ROOT_DIRECTORY_SYMBOL)
+    current_directory = root
+    for command in commands:
+        if command.startswith(consts.CHANGE_DIRECTORY_COMMAND):
+            current_directory = input_parsing.get_next_directory(
+                current_directory, command
+            )
+        elif command.startswith(consts.LIST_CONTENTS_COMMAND):
+            input_parsing.handle_ls_command(current_directory, command)
+
+    return root
 
 
 def get_answer(input_text: str):
