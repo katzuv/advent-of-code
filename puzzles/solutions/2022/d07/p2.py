43dfb1e6eed4cd8d6074f15811c36cb3f1b62b93
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-17 13:19:02 +0200

    Add function that size of the smallest directory that should be deleted

--- puzzles/solutions/2022/d07/p2.py
+++ puzzles/solutions/2022/d07/p2.py
@@ -1,8 +1,24 @@
 import sys
 
+import consts
+import p1
+
 
 def get_answer(input_text: str):
-    raise NotImplementedError
+    """Return total size of the smallest directory that, if deleted, would free up enough space on the filesystem."""
+    filesystem = p1.get_filesystem(input_text)
+    root = filesystem[0]
+    total_used_space = root.size
+    unused_space_needed = consts.REQUIRED_UNUSED_SPACE - (
+        consts.TOTAL_DISK_SPACE - total_used_space
+    )
+    candidate_directories = (
+        directory for directory in filesystem if directory.size >= unused_space_needed
+    )
+    directory_to_delete = min(
+        candidate_directories, key=lambda directory: directory.size
+    )
+    return directory_to_delete.size
 
 
 if __name__ == "__main__":
