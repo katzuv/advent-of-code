eb7e2e8a77e960c5a4436403861da9e73621bc1d
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-17 13:34:36 +0200

    Add function that creates the trees map from the input

--- puzzles/solutions/2022/d08/p1.py
+++ puzzles/solutions/2022/d08/p1.py
@@ -1,6 +1,14 @@
 import sys
 
 
+def get_trees_map(input_text: str) -> tuple[tuple[int, ...], ...]:
+    """
+    :param input_text: puzzle input
+    :return: tuple of tuples representing the map of the tress
+    """
+    return tuple(tuple(map(int, row)) for row in input_text.splitlines())
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
