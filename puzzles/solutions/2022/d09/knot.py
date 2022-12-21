e20b6844fd0dea14b71b46fc8aff51974a500dd3
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-21 17:32:10 +0200

    Add method that moves a knot

--- puzzles/solutions/2022/d09/knot.py
+++ puzzles/solutions/2022/d09/knot.py
@@ -7,3 +7,12 @@ class Knot:
 
     row: int
     column: int
+
+    def move(self, step: tuple[int, int]) -> None:
+        """
+        Move the knot by the given step.
+        :param step: pair of (row, column) movement
+        """
+        x, y = step
+        self.row += x
+        self.column += y
