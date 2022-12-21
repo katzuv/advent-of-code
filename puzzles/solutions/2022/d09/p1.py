c6e7c1e1059e2eafe1e60108cfdab75aa113082d
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-21 17:21:15 +0200

    Add function that creates a sequence of steps from the input

--- puzzles/solutions/2022/d09/p1.py
+++ puzzles/solutions/2022/d09/p1.py
@@ -1,4 +1,21 @@
 import sys
+from typing import Iterator
+
+import consts
+
+
+def get_steps(input_text: str) -> Iterator[tuple[int, int]]:
+    """
+    :param input_text: puzzle input
+    :return: sequence of steps, one by one
+    """
+    steps = []
+    for step_count in input_text.splitlines():
+        direction, count = step_count.split()
+        step = consts.DIRECTION_TO_STEP[direction]
+        count = int(count)
+        yield from count * [step]
+    return tuple(steps)
 
 
 def get_answer(input_text: str):
