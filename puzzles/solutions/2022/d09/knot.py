697a6b982e3b9f00790b1c064a864cbab68bbe76
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-23 16:17:52 +0200

    Add method that moves a knot to be touching another knot

--- puzzles/solutions/2022/d09/knot.py
+++ puzzles/solutions/2022/d09/knot.py
@@ -1,4 +1,5 @@
 import dataclasses
+import math
 
 from typing import Self
 
@@ -30,3 +31,27 @@ class Knot:
             abs(self.row - other.row) < consts.FAR_DISTANCE
             and abs(self.column - other.column) < consts.FAR_DISTANCE
         )
+
+    def move_to_other(self, other: Self) -> None:
+        """
+        Move this knot, so it will be touching the other knot.
+        :param other: other knot to move towards.
+        """
+        if self.is_touching(other):
+            return
+
+        row_distance = self.row - other.row
+        column_distance = self.column - other.column
+
+        # If the distance is "far", we want to move the knot, so it will be on the same row/column, but not overlapping,
+        #   so the distance to move should be 1.
+        # If the distance is "adjacent", it means we want to move diagonally (remember -- the knots are not touching,
+        #   otherwise, we would return immediately at the beginning of this method), so the distance to move
+        #   stays 1, too.
+        # If the distance is 0, no movement should occur, the condition is falsy, so the distance to move stays 0.
+        if abs(row_distance) == consts.FAR_DISTANCE:
+            row_distance = math.copysign(consts.ADJACENT_DISTANCE, row_distance)
+        if abs(column_distance) == consts.FAR_DISTANCE:
+            column_distance = math.copysign(consts.ADJACENT_DISTANCE, column_distance)
+
+        self.move((-int(row_distance), -int(column_distance)))
