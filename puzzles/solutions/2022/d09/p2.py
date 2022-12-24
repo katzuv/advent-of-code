66aaa28f5b172b1a8f9d8afa95aeeaac4999afa2
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-24 20:14:38 +0200

    Add a function that returns number of positions tail of the rope visits
    This is for a rope with ten knots.

--- puzzles/solutions/2022/d09/p2.py
+++ puzzles/solutions/2022/d09/p2.py
@@ -1,8 +1,12 @@
 import sys
 
+import p1
+
 
 def get_answer(input_text: str):
-    raise NotImplementedError
+    """Return the number of positions the tail of the rope visits at least once, when the rope has total 10 knots."""
+    steps = p1.get_steps(input_text)
+    return p1.get_visited_positions_amount(steps, 10)
 
 
 if __name__ == "__main__":
