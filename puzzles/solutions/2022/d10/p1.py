66b8a0de8b9b692b09135c8f308348d145ea8283
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-25 13:51:19 +0200

    Add function that yields instructions from the input

--- puzzles/solutions/2022/d10/p1.py
+++ puzzles/solutions/2022/d10/p1.py
@@ -1,4 +1,15 @@
 import sys
+from typing import Iterator
+
+
+def get_instructions(input_text: str) -> Iterator[tuple[str, tuple[int, ...]]]:
+    """
+    :param input_text: puzzle input
+    :return: generator of instructions and their parameters
+    """
+    for line in input_text.splitlines():
+        instruction, *parameters = line.split()
+        yield instruction, tuple(map(int, parameters))
 
 
 def get_answer(input_text: str):
