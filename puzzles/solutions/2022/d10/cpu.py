88c84f3460e267b89af9b0d24f2e186211a554ee
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-12-25 13:13:48 +0200

    Add constructor to `CPU` class

--- puzzles/solutions/2022/d10/cpu.py
+++ puzzles/solutions/2022/d10/cpu.py
@@ -3,3 +3,14 @@ class CPU:
 
     _INITIAL_INTERESTING_CYCLE = 20
     _INTERESTING_CYCLES_DIFFERENCE = 40
+
+    def __init__(self):
+        """Instantiate a CPU."""
+        self._x_register = 1
+
+        self._cycle = 0
+        self._cycle_in_instruction = 0
+        self.is_ready_for_next = False
+
+        self._interesting_cycle = self._INITIAL_INTERESTING_CYCLE
+        self.interesting_signal_strengths_sum = 0
