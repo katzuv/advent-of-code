40601f667b712034e9d86566fcfa8294240fc2a3
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-11 19:58:53 +0200

    Add constructor to `HandheldDevice` class

--- puzzles/solutions/2022/d10/handheld_device.py
+++ puzzles/solutions/2022/d10/handheld_device.py
@@ -1,3 +1,6 @@
+from cpu import CPU
+
+
 class HandheldDevice:
     """The handheld device, which has a CPU and a CRT screen."""
 
@@ -6,3 +9,10 @@ class HandheldDevice:
 
     _DARK_PIXEL = "."
     _LIT_PIXEL = "#"
+
+    def __init__(self):
+        """Instantiate a handheld device."""
+        self._cpu = CPU()
+        self._screen = [
+            [self._DARK_PIXEL] * self._SCREEN_WIDTH for _ in range(self._SCREEN_HEIGHT)
+        ]
