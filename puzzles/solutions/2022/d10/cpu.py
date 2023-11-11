d6215409ef1bf908d5dfc0e56a5d3f25d549f5f3
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-11 19:16:53 +0200

    Add method that runs a given instruction and calculates signal strength

--- puzzles/solutions/2022/d10/cpu.py
+++ puzzles/solutions/2022/d10/cpu.py
@@ -18,6 +18,23 @@ class CPU:
         self._interesting_cycle = self._INITIAL_INTERESTING_CYCLE
         self.interesting_signal_strengths_sum = 0
 
+    def run(self, opcode: str, parameters: Sequence[int]) -> None:
+        """
+        Run the given instruction.
+
+        This method also calculates the signal strength at interesting cycles.
+        :param opcode: instruction opcode
+        :param parameters: instruction parameters
+        """
+        self._cycle += 1
+        if self._cycle == self._interesting_cycle:
+            signal_strength = self._cycle * self._x_register
+            self.interesting_signal_strengths_sum += signal_strength
+            self._interesting_cycle += self._INTERESTING_CYCLES_DIFFERENCE
+
+        handler = self._INSTRUCTION_TO_HANDLER[opcode]
+        handler(self, parameters)
+
     def _finish_instruction(self) -> None:
         """Set the cycle in instruction counter to zero and mark the CPU as ready for the next instruction."""
         self._cycle_in_instruction = 0
