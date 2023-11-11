227306ceff9e2315989249104b963c20cfae9172
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-11 19:13:52 +0200

    Add method that handles the `noop` instruction

--- puzzles/solutions/2022/d10/cpu.py
+++ puzzles/solutions/2022/d10/cpu.py
@@ -1,3 +1,6 @@
+from typing import Sequence
+
+
 class CPU:
     """The handheld device's CPU."""
 
@@ -19,3 +22,7 @@ class CPU:
         """Set the cycle in instruction counter to zero and mark the CPU as ready for the next instruction."""
         self._cycle_in_instruction = 0
         self.is_ready_for_next = True
+
+    def _noop(self, parameters: Sequence[int]) -> None:
+        """Handle `noop` instruction."""
+        self._finish_instruction()
