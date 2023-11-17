59ef6b4f73bbb8b0ce3d3429e30976fa3aa34986
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-18 00:24:18 +0200

    Add function that return image drawn onscreen after program is run

--- puzzles/solutions/2022/d10/p2.py
+++ puzzles/solutions/2022/d10/p2.py
@@ -1,8 +1,25 @@
 import sys
 
+from handheld_device import HandheldDevice
+import p1
+
 
 def get_answer(input_text: str):
-    raise NotImplementedError
+    """Return the image drawn on the device's screen after the program is run."""
+    device = HandheldDevice()
+    instructions = p1.get_instructions(input_text)
+    instruction = next(instructions)
+    while True:
+        device.draw_current_pixel_if_needed()
+        instruction_opcode, parameters = instruction
+        device.run(instruction_opcode, parameters)
+        if device.is_ready_for_next:
+            try:
+                instruction = next(instructions)
+            except StopIteration:
+                break
+
+    return device.render_screen()
 
 
 if __name__ == "__main__":
