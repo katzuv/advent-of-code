0493361d4a76b45c0b0d3c58e9ccfe2bef892de6
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-19 18:53:15 +0200

    Add constructor to `KeepAway` class

--- puzzles/solutions/2022/d11/keep_away.py
+++ puzzles/solutions/2022/d11/keep_away.py
@@ -1,2 +1,8 @@
+from monkey import Monkey
+
+
 class KeepAway:
     """Class managing the monkeys' Keep Away game."""
+
+    def __init__(self, monkeys: list[Monkey]):
+        self._monkeys = monkeys
