a495ca6b2ec176e3739c3364f96201a6044195c6
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-19 18:54:44 +0200

    Add relief worry reduction factor class constant

--- puzzles/solutions/2022/d11/keep_away.py
+++ puzzles/solutions/2022/d11/keep_away.py
@@ -4,5 +4,7 @@ from monkey import Monkey
 class KeepAway:
     """Class managing the monkeys' Keep Away game."""
 
+    _RELIEF_WORRY_REDUCTION_FACTOR = 3
+
     def __init__(self, monkeys: list[Monkey]):
         self._monkeys = monkeys
