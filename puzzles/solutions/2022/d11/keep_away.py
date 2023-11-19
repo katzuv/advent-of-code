46cec6ecea0985742e5284b02dd3590882c568b2
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-19 18:58:58 +0200

    class game amount of rounds class constant

--- puzzles/solutions/2022/d11/keep_away.py
+++ puzzles/solutions/2022/d11/keep_away.py
@@ -5,6 +5,7 @@ class KeepAway:
     """Class managing the monkeys' Keep Away game."""
 
     _RELIEF_WORRY_REDUCTION_FACTOR = 3
+    _ROUNDS_AMOUNT = 20
 
     def __init__(self, monkeys: list[Monkey]):
         self._monkeys = monkeys
