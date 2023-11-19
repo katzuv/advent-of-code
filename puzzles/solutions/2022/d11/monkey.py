ec4bac9fdfb722399e5d126dde10be082e2df0ef
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-19 18:41:41 +0200

    Add method to `Monkey` that throws an item to another `Monkey`

--- puzzles/solutions/2022/d11/monkey.py
+++ puzzles/solutions/2022/d11/monkey.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Callable
+from typing import Callable, Self
 
 
 @dataclasses.dataclass
@@ -12,3 +12,14 @@ class Monkey:
     true_test_result_monkey_number: int
     false_test_result_monkey_number: int
     inspected_items_amount: int = 0
+
+    def throw(self, other: Self, item_to_throw: int) -> None:
+        """
+        Throw the given item to another monkey.
+
+        This method also increments the items inspected amount (every inspected item is eventually thrown).
+        :param other: other monkey to throw item to
+        :param item_to_throw: item to throw
+        """
+        other.items.append(item_to_throw)
+        self.inspected_items_amount += 1
