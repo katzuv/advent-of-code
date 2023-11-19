f46b43836cfc50cbf1ec4a90df748b30477f1a7c
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-19 18:56:25 +0200

    Add method to `KeepAway` that handles a single item throwing

--- puzzles/solutions/2022/d11/keep_away.py
+++ puzzles/solutions/2022/d11/keep_away.py
@@ -8,3 +8,19 @@ class KeepAway:
 
     def __init__(self, monkeys: list[Monkey]):
         self._monkeys = monkeys
+
+    def _handle_item(self, item: int, monkey: Monkey) -> None:
+        """
+        Handle a single item -- calculate its worry level and determine the monkey to which the item will be thrown.
+        :param item: item to handle
+        :param monkey: monkey who plays the current turn
+        """
+        worry_level = item
+        worry_level = monkey.worry_function(worry_level)
+        worry_level //= self._RELIEF_WORRY_REDUCTION_FACTOR
+        monkey_number_to_throw_item_to = (
+            monkey.true_test_result_monkey_number
+            if worry_level % monkey.test_divisor == 0
+            else monkey.false_test_result_monkey_number
+        )
+        monkey.throw(self._monkeys[monkey_number_to_throw_item_to], worry_level)
