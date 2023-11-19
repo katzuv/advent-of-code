ad8a578eaee3fafe4882d7a0abe3e66e8692f980
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-19 18:39:15 +0200

    Add fields to `Monkey` class

--- puzzles/solutions/2022/d11/monkey.py
+++ puzzles/solutions/2022/d11/monkey.py
@@ -1,6 +1,14 @@
 import dataclasses
+from typing import Callable
 
 
 @dataclasses.dataclass
 class Monkey:
     """Class representing a monkey playing Keep Away."""
+
+    items: list[int]
+    worry_function: Callable[[int], int]
+    test_divisor: int
+    true_test_result_monkey_number: int
+    false_test_result_monkey_number: int
+    inspected_items_amount: int = 0
