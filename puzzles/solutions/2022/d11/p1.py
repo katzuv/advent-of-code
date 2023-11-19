b9d73c4c419ede34a05303085cef4d7423426a06
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-19 18:49:31 +0200

    Add a function that creates `Monkey`s from the puzzle input

--- puzzles/solutions/2022/d11/p1.py
+++ puzzles/solutions/2022/d11/p1.py
@@ -1,4 +1,19 @@
 import sys
+from monkey import Monkey
+from monkey_creator import create_monkey
+
+
+def create_monkeys(input_text: str) -> list[Monkey]:
+    """
+    :param input_text: puzzle input
+    :return: list of monkeys
+    """
+    monkeys = []
+    for monkey_attributes in input_text.split("\n\n"):
+        monkey_attributes = tuple(map(str.strip, monkey_attributes.splitlines()))
+        monkey = create_monkey(monkey_attributes[1:])
+        monkeys.append(monkey)
+    return monkeys
 
 
 def get_answer(input_text: str):
