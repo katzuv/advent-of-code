9577b9e17037480e0566c72aad9918a649b77753
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-11-25 22:58:03 +0200

    Create worry level modifier function for part 2 using CRT
    The PATH modification is needed to be able to use the crt.py module from the utils package.

--- puzzles/solutions/2022/d11/p2.py
+++ puzzles/solutions/2022/d11/p2.py
@@ -1,4 +1,21 @@
 import sys
+from pathlib import Path
+from typing import Iterable
+
+from monkey import Monkey
+
+
+SOLUTIONS_DIRECTORY = str(Path(__file__).resolve().parents[2])
+sys.path.insert(0, SOLUTIONS_DIRECTORY)
+from utils import crt
+
+
+def modify_worry_level(worry_level: int, monkeys: Iterable[Monkey]) -> int:
+    moduli_to_remainders = {
+        current_monkey.test_divisor: worry_level % current_monkey.test_divisor
+        for current_monkey in monkeys
+    }
+    return crt.calculate_chinese_remainder_theorem(moduli_to_remainders)
 
 
 def get_answer(input_text: str):
