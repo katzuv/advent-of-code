9b12179f8eb8fd95b7c8b30ddfd00009fbf77ce3
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-12-02 13:47:16 +0200

    Add function that parses games info from the input

--- puzzles/solutions/2023/d02/p1.py
+++ puzzles/solutions/2023/d02/p1.py
@@ -1,6 +1,23 @@
 import sys
 
 
+def get_games_info(input_text: str) -> list[[list[dict[str, int]]]]:
+    games_info = []
+    for line in input_text.splitlines():
+        game_info = []
+        rounds = line.split(": ")[1].split("; ")
+        for round_info in rounds:
+            round_color_amounts = {}
+            color_amounts = round_info.split(", ")
+            for color in color_amounts:
+                amount, color = color.split()
+                round_color_amounts[color] = int(amount)
+            game_info.append(round_color_amounts)
+        games_info.append(game_info)
+
+    return games_info
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
