ce1f57065df2648dfabee5264d12307f934a1b67
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-12-02 14:38:16 +0200

    Pass search start index instead of creating another variable

--- puzzles/solutions/2023/d01/p2.py
+++ puzzles/solutions/2023/d01/p2.py
@@ -16,10 +16,9 @@ def _convert_digits_names_to_numbers(input_text: str) -> str:
     for line in input_text.splitlines():
         new_line = line[0]
         for i in range(1, len(line)):
-            current_part_of_line = line[: i + 1]
             new_line += line[i]
             for digit_name in DIGITS_NAMES_TO_NUMBERS:
-                if current_part_of_line.endswith(digit_name):
+                if line.endswith(digit_name, 0, i + 1):
                     new_line += DIGITS_NAMES_TO_NUMBERS[digit_name]
         new_text += new_line + "\n"
     return new_text
