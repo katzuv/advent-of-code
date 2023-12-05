38e43480d1b8b8f10d180ee27fad4d64dff56aac
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-12-05 11:10:40 +0200

    Create function that returns a coordinate from a given position

--- puzzles/solutions/2022/d12/input_parser.py
+++ puzzles/solutions/2022/d12/input_parser.py
@@ -4,3 +4,16 @@ import collections
 Coordinate = collections.namedtuple(
     "Coordinate", ("row", "column", "elevation", "is_start", "is_end")
 )
+
+
+def _create_coordinate(heightmap: list[str], row: int, column: int) -> Coordinate:
+    elevation = heightmap[row][column]
+    is_start = elevation == "S"
+    is_end = elevation == "E"
+    if is_start:
+        elevation = ord("a")
+    elif is_end:
+        elevation = ord("z") + 1
+    else:
+        elevation = ord(elevation)
+    return Coordinate(row, column, elevation, is_start, is_end)
