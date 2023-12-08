a1a42f2d7c1603c11f1d2dea549e1f22f4fd8b2a
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-12-09 00:09:59 +0200

    Add constructor to `SchematicParser` class

--- puzzles/solutions/2023/d03/schematic_parser.py
+++ puzzles/solutions/2023/d03/schematic_parser.py
@@ -1,2 +1,9 @@
 class SchematicParser:
     """Class that parsers the engine schematic."""
+
+    def __init__(self, engine_schematic: str):
+        self._schematic = engine_schematic.splitlines()
+        self._schematic_length = len(self._schematic)
+        self._schematic_width = len(self._schematic[0])
+
+        self._numbers = []
