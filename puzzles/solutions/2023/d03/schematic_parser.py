9ee5c0257527933e2b0ea977af03d1914169cea0
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-12-09 00:19:46 +0200

    Add method that checks whether character is adjacent to other characters

--- puzzles/solutions/2023/d03/schematic_parser.py
+++ puzzles/solutions/2023/d03/schematic_parser.py
@@ -1,3 +1,7 @@
+import itertools
+from typing import Iterable
+
+
 class SchematicParser:
     """Class that parsers the engine schematic."""
 
@@ -7,3 +11,35 @@ class SchematicParser:
         self._schematic_width = len(self._schematic[0])
 
         self._numbers = []
+
+    def _is_character_adjacent_to_character(
+        self,
+        character_row: int,
+        character_column: int,
+        characters: Iterable[str],
+        should_match: bool,
+    ) -> bool:
+        """
+        :param character_row: row of the checked character
+        :param character_column: column of the checked character
+        :param characters: characters to check against
+        :param should_match: should the character be adjacent one of the given characters or not
+        :return: whether the character is (not) adjacent to the given characters
+        """
+        upper_row = max(0, character_row - 1)
+        lower_row = min(self._schematic_length - 1, character_row + 1)
+        left_column = max(0, character_column - 1)
+        right_column = min(self._schematic_width - 1, character_column + 1)
+
+        neighbors = list(
+            itertools.product(
+                range(upper_row, lower_row + 1), range(left_column, right_column + 1)
+            )
+        )
+        neighbors.remove((character_row, character_column))
+        for row, column in neighbors:
+            character = self._schematic[row][column]
+            if (character in characters) == should_match:
+                return True
+
+        return False
