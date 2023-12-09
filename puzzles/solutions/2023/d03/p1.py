6dc00f0f0fe76e9501eb815fa42e22f7ad721e78
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-12-10 00:04:14 +0200

    Add function that returns sum of part numbers in the engine schematic

--- puzzles/solutions/2023/d03/p1.py
+++ puzzles/solutions/2023/d03/p1.py
@@ -1,8 +1,13 @@
 import sys
 
+from schematic_parser import SchematicParser
+
 
 def get_answer(input_text: str):
-    raise NotImplementedError
+    """Return the sum of all part numbers in the engine schematic."""
+    schematic_parser = SchematicParser(input_text)
+    part_numbers = schematic_parser.find_part_numbers()
+    return sum(part_numbers)
 
 
 if __name__ == "__main__":
