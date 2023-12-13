4c196d90678c5396c4b4fc5300ad553ba32d9ae9
Dan Katzuv <dan.dan.kafe@gmail.com>
2023-12-13 23:46:44 +0200

    Create function that returns mapping ranges

--- puzzles/solutions/2023/d05/p1.py
+++ puzzles/solutions/2023/d05/p1.py
@@ -1,6 +1,21 @@
 import sys
 
 
+def get_map_ranges(lines: list[str]) -> dict[int, int]:
+    mapping = {}
+    for line in lines:
+        destination_range_start, source_range_start, range_length = map(
+            int, line.split()
+        )
+        source_numbers = range(source_range_start, source_range_start + range_length)
+        destination_numbers = range(
+            destination_range_start, destination_range_start + range_length
+        )
+
+        mapping |= zip(source_numbers, destination_numbers)
+    return mapping
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
