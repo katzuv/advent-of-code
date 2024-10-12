59be607bc6c85c604be0a37897f7a23db97582c9
Dan Katzuv <dan.dan.kafe@gmail.com>
2024-10-12 14:03:36 +0300

    Create new `Mapping` type hint

--- puzzles/solutions/2023/d05/input_parsing.py
+++ puzzles/solutions/2023/d05/input_parsing.py
@@ -1,7 +1,10 @@
 import re
 
 
-def get_map_ranges(lines: list[str]) -> list[tuple[int, int, int]]:
+Mapping = list[tuple[int, int, int]]
+
+
+def get_map_ranges(lines: list[str]) -> Mapping:
     ranges = []
     for line in lines:
         destination_range_start, source_range_start, range_length = map(
@@ -13,14 +16,14 @@ def get_map_ranges(lines: list[str]) -> list[tuple[int, int, int]]:
     return ranges
 
 
-def get_category_mapping(mapping: str) -> tuple[str, list[tuple[int, int, int]]]:
+def get_category_mapping(mapping: str) -> tuple[str, Mapping]:
     lines = mapping.splitlines()
     source = re.match(r"([a-z]+)", lines[0]).group()
     ranges = get_map_ranges(lines[1:])
     return source, ranges
 
 
-def get_all_mappings(mapping_lines: str) -> dict[str, list[tuple[int, int, int]]]:
+def get_all_mappings(mapping_lines: str) -> dict[str, Mapping]:
     source_to_destination_categories_ranges = {}
     for mapping in mapping_lines.split("\n\n"):
         source, ranges = get_category_mapping(mapping)
@@ -29,8 +32,8 @@ def get_all_mappings(mapping_lines: str) -> dict[str, list[tuple[int, int, int]]
 
 
 def get_initial_data(
-        input_text: str,
-) -> tuple[list[int], dict[str, list[tuple[int, int, int]]]]:
+    input_text: str,
+) -> tuple[list[int], dict[str, Mapping]]:
     first_line, mapping_lines = input_text.split("\n\n", maxsplit=1)
     initial_seeds = [int(number) for number in re.findall(r"\d+", first_line)]
     mappings = get_all_mappings(mapping_lines)
