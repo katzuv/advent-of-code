3767bf893b18a1f5da7a91d5a61bc0968cc23e4d
Dan Katzuv <dan.dan.kafe@gmail.com>
2024-10-12 13:55:06 +0300

    Extract input parsing functions into another module

--- puzzles/solutions/2023/d05/p1.py
+++ puzzles/solutions/2023/d05/p1.py
@@ -1,47 +1,11 @@
 import math
-import re
 import sys
-
-
-def get_map_ranges(lines: list[str]) -> list[tuple[int, int, int]]:
-    ranges = []
-    for line in lines:
-        destination_range_start, source_range_start, range_length = map(
-            int, line.split()
-        )
-        difference = destination_range_start - source_range_start
-        source_range_end = source_range_start + range_length
-        ranges.append((source_range_start, source_range_end, difference))
-    return ranges
-
-
-def get_category_mapping(mapping: str) -> tuple[str, list[tuple[int, int, int]]]:
-    lines = mapping.splitlines()
-    source = re.match(r"([a-z]+)", lines[0]).group()
-    ranges = get_map_ranges(lines[1:])
-    return source, ranges
-
-
-def get_all_mappings(mapping_lines: str) -> dict[str, list[tuple[int, int, int]]]:
-    source_to_destination_categories_ranges = {}
-    for mapping in mapping_lines.split("\n\n"):
-        source, ranges = get_category_mapping(mapping)
-        source_to_destination_categories_ranges[source] = ranges
-    return source_to_destination_categories_ranges
-
-
-def get_initial_data(
-    input_text: str,
-) -> tuple[list[int], dict[str, list[tuple[int, int, int]]]]:
-    first_line, mapping_lines = input_text.split("\n\n", maxsplit=1)
-    initial_seeds = [int(number) for number in re.findall(r"\d+", first_line)]
-    mappings = get_all_mappings(mapping_lines)
-    return initial_seeds, mappings
+import input_parsing
 
 
 def get_answer(input_text: str) -> int:
     """Return the lowest location number hat corresponds to any of the initial seed numbers."""
-    initial_seeds, mappings = get_initial_data(input_text)
+    initial_seeds, mappings = input_parsing.get_initial_data(input_text)
     lowest_location_number = math.inf
     for seed in initial_seeds:
         running_value = seed
