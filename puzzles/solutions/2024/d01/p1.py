fcacab414701023f5ef9464c04e826aa9da7d854
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-10-23 20:27:00 +0300

    Add function that returns the location ID lists

--- puzzles/solutions/2024/d01/p1.py
+++ puzzles/solutions/2024/d01/p1.py
@@ -1,6 +1,15 @@
 import sys
 
 
+def get_location_id_lists(input_text: str) -> tuple[list[int], list[int]]:
+    first, second = [], []
+    for row in input_text.splitlines():
+        first_id, second_id = row.split()
+        first.append(int(first_id))
+        second.append(int(second_id))
+    return first, second
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
