eb734969d74774bc7e0a8c1712be53410c6179a5
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-10-23 20:25:54 +0300

    Create solution file for part 1

---
+++ puzzles/solutions/2024/d01/p1.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def get_answer(input_text: str):
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    try:
+        print(get_answer(sys.argv[1]))
+    except IndexError:
+        pass  # Don't crash if no input was passed through command line arguments.
