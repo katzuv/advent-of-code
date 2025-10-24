46c4df733c98157b9e13804d71c269e707eb37bc
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-10-24 13:22:06 +0300

    Check if report is safe

--- puzzles/solutions/2024/d02/p1.py
+++ puzzles/solutions/2024/d02/p1.py
@@ -1,8 +1,20 @@
+import itertools
 import sys
 
 
 def get_reports(input_text: str) -> list[tuple[int, ...]]:
     return [tuple(map(int, line.split())) for line in input_text.splitlines()]
+
+
+def is_report_safe(report: tuple[int, ...]) -> bool:
+    increasing = (report[1] - report[0]) > 0
+    for current_level, next_level in itertools.pairwise(report):
+        difference = next_level - current_level
+        if (difference > 0) != increasing:  # Direction changed.
+            return False
+        if not (1 <= abs(difference) <= 3):  # Difference is not in range.
+            return False
+    return True
 
 
 def get_answer(input_text: str):
