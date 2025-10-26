7b08a52811e86c83c7fa74c0b6a8d90ea2938f79
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-10-27 00:52:29 +0200

    Get all `A` locations

--- puzzles/solutions/2024/d04/p2.py
+++ puzzles/solutions/2024/d04/p2.py
@@ -1,5 +1,15 @@
 import sys
 
+from p1 import Matrix
+
+
+def get_all_a_locations(matrix: Matrix) -> list[tuple[int, int]]:
+    return [
+        (row, column)
+        for row, column in itertools.product(range(len(matrix)), range(len(matrix[0])))
+        if matrix[row][column] == "A"
+    ]
+
 
 def get_answer(input_text: str):
     raise NotImplementedError
