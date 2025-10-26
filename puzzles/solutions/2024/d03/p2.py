0756e36629ec91df6bc1e227afcdfa4900385a22
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-10-26 19:10:00 +0200

    Sum only relevant section

--- puzzles/solutions/2024/d03/p2.py
+++ puzzles/solutions/2024/d03/p2.py
@@ -1,8 +1,19 @@
 import sys
 
+from p1 import get_products_sum
 
-def get_answer(input_text: str):
-    raise NotImplementedError
+
+def get_relevant_sections(input_text: str) -> list[str]:
+    sections = input_text.split("do")
+    # At the beginning of the program, `mul` instructions are enabled.
+    return [sections[0]] + [
+        section for section in sections[1:] if section.startswith("()")
+    ]
+
+
+def get_answer(input_text: str) -> int:
+    complete_text = "".join(get_relevant_sections(input_text))
+    return get_products_sum(complete_text)
 
 
 if __name__ == "__main__":
