2a5b55cb170325353e54929e400b517fa3ad83b6
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-10-29 00:13:19 +0200

    Check if update is in right order

--- puzzles/solutions/2024/d05/p1.py
+++ puzzles/solutions/2024/d05/p1.py
@@ -11,6 +11,16 @@ def get_ordering_rules(ordering_rules: str) -> tuple[int, ...]:
     return tuple(graph.static_order())
 
 
+def is_update_in_right_order(order: tuple[int, ...], update: list[str]) -> bool:
+    previous_page_number_index = -1
+    for page_number in update:
+        current_page_number_index = order.index(page_number)
+        if current_page_number_index < previous_page_number_index:
+            return False
+        previous_page_number_index = current_page_number_index
+    return True
+
+
 def get_answer(input_text: str):
     raise NotImplementedError
 
