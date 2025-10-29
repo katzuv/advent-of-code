b272a31084cef8b27611331fe902fa5ff1c7cb2e
Dan Katzuv <dan.dan.kafe@gmail.com>
2025-10-29 18:52:33 +0200

    Sort incorrectly-ordered updates

--- puzzles/solutions/2024/d05/p2.py
+++ puzzles/solutions/2024/d05/p2.py
@@ -1,5 +1,23 @@
 import sys
 
+from p1 import Graph
+
+
+def sort_update(update: list[str], order: Graph) -> list[str]:
+    page_numbers = set(update)
+    needed_order = {
+        page_number: order[page_number] & page_numbers for page_number in update
+    }
+    sorted_update = []
+    while len(sorted_update) < len(update):
+        for page_number in update:
+            if page_number in sorted_update:
+                continue
+            dependencies = needed_order[page_number]
+            if dependencies <= set(sorted_update):
+                sorted_update.append(page_number)
+    return sorted_update
+
 
 def get_answer(input_text: str):
     raise NotImplementedError
