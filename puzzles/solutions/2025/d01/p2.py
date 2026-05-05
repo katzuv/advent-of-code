781d6599e5c417fa86afc49245b31a0f1f1afd32
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-05-06 01:12:05 +0300

    Solve day 1 part 2

--- puzzles/solutions/2025/d01/p2.py
+++ puzzles/solutions/2025/d01/p2.py
@@ -10,6 +10,32 @@ def get_answer(input_text: str):
 
     visits_at_zero = 0
 
+    # Because of how division works with negative numbers, we need to split the logic between going left and right.
+    for turn in turns:
+        # If everything's positive, we can use regular floor division.
+        if turn > 0:
+            times_at_zero = (current_turn + turn) // 100
+
+        else:
+            # Take the additive inverse for simpler math.
+            steps = -turn
+            dist_to_first_zero = 100 if current_turn == 0 else current_turn
+            # If we have "enough" steps to hit the first zero...
+            if steps >= dist_to_first_zero:
+                # We visited a zero for the first time.
+                visits_at_zero += 1
+                # Count how many zeros our steps can hit after the first zero.
+                times_at_zero = (steps - dist_to_first_zero) // 100
+            # Not enough steps to hit the first zero.
+            else:
+                times_at_zero = 0
+
+        # Fortunately this works fine for negative numbers as well, so no need to split.
+        current_turn = (current_turn + turn) % 100
+        visits_at_zero += times_at_zero
+
+    return visits_at_zero
+
 
 if __name__ == "__main__":
     try:
