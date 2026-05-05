2917997c9d8d775b5b71cc4fdb2b35092adf59af
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-05-05 22:12:32 +0300

    Parse turns from input

--- puzzles/solutions/2025/d01/p1.py
+++ puzzles/solutions/2025/d01/p1.py
@@ -1,8 +1,19 @@
 import sys
 
 
+def get_turns(input_text: str) -> list[int]:
+    turns = []
+    for turn in input_text.splitlines():
+        direction = turn[0]
+        steps = int(turn[1:])
+        if direction == "L":
+            steps *= -1
+        turns.append(steps)
+    return turns
+
+
 def get_answer(input_text: str):
-    raise NotImplementedError
+    turns = get_turns(input_text)
 
 
 if __name__ == "__main__":
