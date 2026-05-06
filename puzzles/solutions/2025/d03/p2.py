2bb9d202e86a0fa9a52050d27538ccf866c005a3
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-05-07 00:43:02 +0300

    Solve part 2

--- puzzles/solutions/2025/d03/p2.py
+++ puzzles/solutions/2025/d03/p2.py
@@ -1,8 +1,25 @@
 import sys
 
+from p1 import get_banks
+
+BATTERIES_AMOUNT = 12
+
 
 def get_answer(input_text: str):
-    raise NotImplementedError
+    banks = get_banks(input_text)
+    max_joltages = 0
+    for bank in banks:
+        max_joltage = 0
+        current_index = 0
+        for digit_number in range(BATTERIES_AMOUNT):
+            last_possible_index = 1 + digit_number - BATTERIES_AMOUNT
+            if last_possible_index == 0:
+                last_possible_index = len(bank)
+            max_digit = max(bank[current_index:last_possible_index])
+            current_index = bank.index(max_digit, current_index) + 1
+            max_joltage += max_digit * 10 ** (BATTERIES_AMOUNT - digit_number - 1)
+        max_joltages += max_joltage
+    return max_joltages
 
 
 if __name__ == "__main__":
