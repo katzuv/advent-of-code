8777436ac67d80efe4e22105a6dcddc9dd63b8cc
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-05-08 00:24:53 +0300

    Sort lists beforehand
    Didn't want to do so before part 2, didn't know if the order mattered. Well, it's needed to be sorted for both parts.
    Also, apparently, no need to sort the available products list.

--- puzzles/solutions/2025/d05/p1.py
+++ puzzles/solutions/2025/d05/p1.py
@@ -24,7 +24,6 @@ def get_products(input_text: str) -> tuple[list[range], list[int]]:
     available_products_parsed = [int(line) for line in available_products.splitlines()]
 
     fresh_products_parsed.sort(key=lambda r: r.start)
-    available_products_parsed.sort()
     return fresh_products_parsed, available_products_parsed
 
 
@@ -41,7 +40,6 @@ def merge_fresh_product_ranges(ranges: list[range]) -> list[range]:
 
 def get_answer(input_text: str):
     fresh_products, available_products = get_products(input_text)
-
     fresh_products = merge_fresh_product_ranges(fresh_products)
     fresh_available_products = []
 
