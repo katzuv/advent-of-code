90bca9b6a778d434928df095a4a1b918f15caf2a
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-05-08 23:24:54 +0300

    Add function that parses list of problems from input

--- puzzles/solutions/2025/d06/p1.py
+++ puzzles/solutions/2025/d06/p1.py
@@ -5,6 +5,24 @@ input_example = """123 328  51 64
   6 98  215 314
 *   +   *   +  """
 
+Problem = tuple[list[int], Callable[[int, int], int]]
+
+
+def get_problems(input_text: str) -> list[Problem]:
+    lines = input_text.splitlines()
+    operators = (
+        (m.start(), m.end(), m.group().strip())
+        for m in re.finditer(r"[+*]\s*", lines[-1])
+    )
+    lines = lines[:-1]
+    return [
+        (
+            [int(line[start:end]) for line in lines],
+            operator.add if operation == "+" else operator.mul,
+        )
+        for (start, end, operation) in operators
+    ]
+
 
 def get_answer(input_text: str):
     raise NotImplementedError
