fc07824aba9d85b67a7530d080ea536858b4a617
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-05-08 23:35:45 +0300

    part Create solution file for part 2

---
+++ puzzles/solutions/2025/d06/p2.py
@@ -0,0 +1,12 @@
+import sys
+
+import p1
+
+
+def get_answer(input_text: str):
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    puzzle_input = sys.argv[1] if len(sys.argv) > 1 else p1.input_example
+    print(get_answer(puzzle_input))
