593cd2abd34eeb0a34b7efde16adf18ad30fde2a
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-05-09 01:16:58 +0300

    Add function that "transposes" the number columns

--- puzzles/solutions/2025/d06/p2.py
+++ puzzles/solutions/2025/d06/p2.py
@@ -1,6 +1,23 @@
+import re
 import sys
 
 import p1
+
+
+def transpose_numbers(number_lines: list[str]) -> list[list[int]]:
+    columns_count = len(number_lines[0])
+    number_lines = [
+        [line[column] for line in number_lines] for column in range(columns_count)
+    ]
+    transposed_number_lines = [[]]
+    for line in number_lines:
+        try:
+            transposed_number_lines[-1].append(int("".join(line)))
+        except ValueError:
+            # Whitespace row, create a new Problem.
+            transposed_number_lines.append([])
+
+    return transposed_number_lines
 
 
 def get_answer(input_text: str):
