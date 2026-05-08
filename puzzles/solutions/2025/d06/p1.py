d533619f4ab24b2f1d7f0c0963df03a9259cba23
Dan Katzuv <dan.dan.kafe@gmail.com>
2026-05-09 00:29:35 +0300

    Revert "Add function that parses numbers from the input text"
    This reverts commit d89618f019073f1272a0bf26006bbbb16bc5fd5c.
    It doesn't work that way...

--- puzzles/solutions/2025/d06/p1.py
+++ puzzles/solutions/2025/d06/p1.py
@@ -14,10 +14,6 @@ Problem = tuple[list[int], Callable[[int, int], int]]
 OPERATIONS_MAP = {"+": operator.add, "*": operator.mul}
 
 
-def split_input_into_number_lines(number_lines: list[str]) -> list[list[int]]:
-    return [[int(number) for number in line.split()] for line in number_lines]
-
-
 def get_problems(input_text: str) -> list[Problem]:
     lines = input_text.splitlines()
     number_lines = lines[:-1]
@@ -35,10 +31,6 @@ def get_answer(input_text: str):
     return sum(
         functools.reduce(operation, numbers) for (numbers, operation) in problems
     )
-
-
-def get_answer(input_text: str):
-    return sum_results(input_text)
 
 
 if __name__ == "__main__":
